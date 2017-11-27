c1da1772198a87ab44db31336ffb0feb3330e9ae
todder02 <tmantell99@yahoo.com>
2017-11-26 21:42:56 -0600

    added initial routing

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,9 +1,15 @@
 import {render, hydrate} from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import ModeratorContainer from '../../moderator/index';
 
 const App = () => (
-	<div style={{color: 'purple', fontWeight: '600'}}>
-		Put app content here.
-	</div>
+	<Router>
+		<Route to="/moderator" component={ModeratorContainer} />
+		
+		<div style={{color: 'purple', fontWeight: '600'}}>
+			Put app content here.
+		</div>
+	</Router>
 );
 
 render(<App />, document.getElementById('root'));
