473cab4ffcd44f20ff2969171b021ed418ca5ed0
Todd Mantell <tmantell99@yahoo.com>
2017-11-26 22:28:06 -0600

    added express setup to serve the app

---
+++ src/server/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('build'));
+
+app.get('/', loggerMiddleware, (req, res) => {
+  res.sendFile(path.resolve('index.html'));
+});
+
+function loggerMiddleware(req, res, next) {
+  console.log('base route accessed');
+  next();
+}
+
+module.exports = app;
