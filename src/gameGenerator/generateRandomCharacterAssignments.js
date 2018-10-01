a955f424d7d8bc63e4ced6aff138ae421387179d
Todd Mantell <tmantell99@yahoo.com>
2018-10-01 10:36:31 -0500

    refactored checking to see if current character is a werewolf or villager for simplicity

--- src/gameGenerator/generateRandomCharacterAssignments.js
+++ src/gameGenerator/generateRandomCharacterAssignments.js
@@ -3,46 +3,53 @@ import { CHARACTERS } from "../constants";
 import findNumberOfWerewolves from "./findNumberOfWerewolves";
 import { characterFactory } from "../characterGenerator/characterFactory";
 
-export default (players, charactersInGame) => {
+const WEREWOLVES = [ CHARACTERS.WEREWOLF, CHARACTERS.WHITE_WOLF, CHARACTERS.BIG_BAD_WOLF, CHARACTERS.DIRE_WOLF ];
+const VILLAGERS = [ CHARACTERS.VILLAGER, CHARACTERS.VIGILANTE ];
+
+const getRandomCharacterAssignments = (players, charactersInGame) => {
 	let availableWerewolves = findNumberOfWerewolves(players, charactersInGame);
 	let availableVillagers = players.length - availableWerewolves;
 	let availablePlayers = [...players];
 	let assignedPlayers = [];
 
-	charactersInGame.forEach(character => {		
-		if (checkForWerewolf(character)) {
+	charactersInGame.forEach(characterName => {		
+		if (WEREWOLVES.includes(characterName)) {
 			while(availableWerewolves) {
 				const randomIndex = Math.floor(Math.random() * availablePlayers.length);
 				const playerToAssign = availablePlayers[randomIndex];
-				playerToAssign.character = characterFactory(character);
+				playerToAssign.character = characterFactory(characterName);
 				availablePlayers = availablePlayers.filter(player => player.id !== playerToAssign.id)			
 				availableWerewolves--;
 				assignedPlayers = [...assignedPlayers, playerToAssign];
 			}
 		}
 
-		if (checkForVillager(character)) {
+		if (VILLAGERS.includes(characterName)) {
 			while(availableVillagers) {
 				const randomIndex = Math.floor(Math.random() * availablePlayers.length);
 				const playerToAssign = availablePlayers[randomIndex];
-				playerToAssign.character = characterFactory(character);
+				playerToAssign.character = characterFactory(characterName);
+				if (checkForSingleUseCharacter) // don't add any more of this type 
 				availablePlayers = availablePlayers.filter(player => player.id !== playerToAssign.id)
 				availableVillagers--;
 				assignedPlayers = [...assignedPlayers, playerToAssign];
 			}
 		}		
 	})
+
+	console.log(JSON.stringify(assignedPlayers))
 	return assignedPlayers;
 }
 
-function checkForWerewolf(characterName) {
-	return characterName === CHARACTERS.WEREWOLF ||
-		characterName === CHARACTERS.WHITE_WOLF ||
-		characterName === CHARACTERS.BIG_BAD_WOLF ||
-		characterName === CHARACTERS.DIRE_WOLF;
+function checkForSingleUseCharacter(characterName) {
+	return characterName.includes(CHARACTERS.VIGILANTE) ||
+	characterName.includes(CHARACTERS.WITCH) ||
+	characterName.includes(CHARACTERS.DIRE_WOLF) ||
+	characterName.includes(CHARACTERS.KNIGHT) ||
+	characterName.includes(CHARACTERS.ANGEL) ||
+	characterName.includes(CHARACTERS.CUPID) ||
+	characterName.includes(CHARACTERS.DEFENDER) ||
+	characterName.includes(CHARACTERS.COLLECTOR);
 }
 
-function checkForVillager(characterName) {
-	return characterName === CHARACTERS.VILLAGER ||
-		characterName === CHARACTERS.VIGILANTE;
-}
+export default getRandomCharacterAssignments;
--- tests/gameGenerator/generateRandomGame.test.js
+++ tests/gameGenerator/generateRandomGame.test.js
@@ -18,6 +18,20 @@ describe('generateRandomGame()', () => {
 		expect(villagerCount).toEqual(3)
 	})
 
+	it('should return two villagers and a vigilante when 5 players and the vigilante are in the game', () => {
+		const playersArray = [{ name: "Todd", id: 1 }, { name: "Karolina", id: 2 }, { name: "Kevin", id: 3 }, { name: "Kasia", id: 4 }, { name: "Jay", id: 5 }];
+		const charactersInGame = [CHARACTERS.VIGILANTE, CHARACTERS.VILLAGER, CHARACTERS.WEREWOLF];
+		const randomlyGeneratedCharacters = generateRandomCharacterAssignments(playersArray, charactersInGame);
+		
+		randomlyGeneratedCharacters.forEach(player => console.log(player.character.name))
+		
+		const vigilante = randomlyGeneratedCharacters.find(player => player.character.name === CHARACTERS.VIGILANTE)
+		const villagers = randomlyGeneratedCharacters.filter(player => player.character.name === CHARACTERS.VILLAGER)		
+		
+		expect(vigilante).toBeTruthy()
+		expect(villagers.length).toEqual(2)
+	});
+
 	it('should return 2 werewolves in a 5 player array', () => {
 		const players = ['Todd', 'Kasia', 'Jay', 'Karolina', 'Kevin'];
 
